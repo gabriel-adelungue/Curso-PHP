e89397117a1171451bd2a43fae7ff5080d0c9bbd
Gabriel Adelungue Cassiano <gabriel.adlungue@gmail.com>
2021-04-27 09:50:38 -0300

    Subindo pastas

---
+++ PDO/exemplo-05.php
@@ -0,0 +1,17 @@
+
+<?php
+
+$conn = new PDO("mysql:dbname=dbphp7;host=localhost", "root","");
+
+$stmt = $conn->prepare("DELETE FROM tb_usuarios WHERE idusuario = :ID");
+
+
+$id = 1;
+
+$stmt->bindParam(":ID", $id);
+
+$stmt->execute();
+
+echo "Delete OK!";
+
+?>
